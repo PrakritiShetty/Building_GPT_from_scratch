49a80fd237c179281b81af5416304b3b140070b7
PrakritiShetty <prakritishetty02@gmail.com>
2024-03-19 12:46:48 +0530

    data import and processing

--- model.py
+++ model.py
@@ -33,3 +33,23 @@ itos = {i:ch for i,ch in enumerate(chars)}
 encode = lambda s: [stoi[c] for c in s]
 # decoder - takes a list of ints, outputs a string
 decode = lambda l:''.join([itos[i] for i in l])
+
+"""Train-Test Split"""
+data = torch.tensor(encode(text),dtype=torch.long)
+# splitting our dataset into train and validation
+n = int(0.9*len(data))
+train_data = data[:n]
+val_data = data[n:]
+
+"""data loading"""
+def get_batch(split):
+  data = train_data if split=='train' else val_data
+  # generating random chunks
+  # randomly offset numbers between len(data) and block_size. Number of such numbers is batch_size
+  ix = torch.randint(len(data) - block_size, (batch_size,))
+  # x is the first block size chars starting at i
+  x = torch.stack([data[i:i+block_size] for i in ix]) 
+  # y is x, but offset by 1
+  y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+  x,y = x, y
+  return x,y
